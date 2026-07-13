18c04ed95c69e045145facb1849a895877cf2a8e
LeoLucasC <gloriaxd77@hotmail.com>
2026-07-13 15:52:01 -0500

    reemplazamanietno 4

--- docker-entrypoint.backend.sh
+++ docker-entrypoint.backend.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "⏳ Esperando a que PostgreSQL esté listo..."
+echo "Esperando BD..."
 
-# Usamos directamente 'db' como host, mucho más seguro y simple
-until pg_isready -h db -U vigamonitor -d vigamonitor; do
+until pg_isready -h db; do
   sleep 1
 done
 
-echo "✅ PostgreSQL está listo"
-
-echo "🌱 Ejecutando seed de base de datos..."
+echo "BD lista"
 python seed.py
 
-echo "🚀 Iniciando servidor..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
